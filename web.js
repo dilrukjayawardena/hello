a28fa662fcdfdac95c0e54aa1336badc76188f17
dushan@codecommit-at-789734661575 <dushan@blackswan-technologies.com>
2023-10-16 18:07:03 +0530

    test com1.

--- web.js
+++ web.js
@@ -1,15 +1,2 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
-
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+// web.js var express = require("express"); var logfmt = require("logfmt"); var app = express(); var k=0sasss; app.use(logfmt.requestLogger()); app.get('/', function(req, res) { res.send('Hello World!'); }); var port = Number(process.env.PORT || 5000);
+app.listen(port, function() { console.log("Listening on " + port); });
